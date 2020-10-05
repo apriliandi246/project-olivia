6e8ca9417da53ab27f5d702069f0e25bd7903332
parhan <apriliandi642@gmail.com>
2020-10-05 22:54:19 +0700

    make search bar for catalog page

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,7 @@
-document.querySelector(".toggle-menu").addEventListener("click", () => {
-   document.querySelector(".navbar__items").classList.toggle("navbar--slide");
-});
+const formSearch = document.querySelector(".product-search");
+const navbarItems = document.querySelector(".navbar__items");
 
-document.querySelector(".toggle-menu").removeEventListener("click", () => {
-   console.log("ok")
-   document.querySelector(".navbar__items").classList.toggle("navbar--slide");
+document.querySelector(".toggle-menu").addEventListener("click", () => {
+   navbarItems.classList.toggle("navbar--slide");
+   navbarItems.classList[1] === "navbar--slide" ? formSearch.style.zIndex = 0 : formSearch.style.zIndex = 1;
 });
