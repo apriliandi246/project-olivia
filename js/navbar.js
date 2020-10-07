3c32e105b0b9a0fa5ffe957ff336463dc634b606
parhan <apriliandi642@gmail.com>
2020-10-08 00:19:03 +0700

    update

--- js/navbar.js
+++
@@ -1,7 +0,0 @@
-const formSearch = document.querySelector(".product-search");
-const navbarItems = document.querySelector(".navbar__items");
-
-document.querySelector(".toggle-menu").addEventListener("click", () => {
-   navbarItems.classList.toggle("navbar--slide");
-   navbarItems.classList[1] === "navbar--slide" ? formSearch.style.zIndex = 0 : formSearch.style.zIndex = 1;
-});
